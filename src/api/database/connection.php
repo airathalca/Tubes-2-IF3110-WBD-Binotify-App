dd80ed978304d594fed52dc9db0d87f172ebe4c9
rayhankinan <rayhankinan@gmail.com>
2022-10-17 15:26:15 +0700

    refactor: remove try except and remove index.php

--- src/api/database/connection.php
+++ src/api/database/connection.php
@@ -9,17 +9,13 @@ function get_connection()
 {
     global $host, $dbname, $user, $password, $user_table, $album_table, $song_table;
 
-    try {
-        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
-        $pdo = new PDO($dsn, (string) $user, (string) $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+    $pdo = new PDO($dsn, (string) $user, (string) $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $pdo->exec($user_table);
-        $pdo->exec($album_table);
-        $pdo->exec($song_table);
+    $pdo->exec($user_table);
+    $pdo->exec($album_table);
+    $pdo->exec($song_table);
 
-        return $pdo;
-    } catch (PDOException $e) {
-        throw $e;
-    }
+    return $pdo;
 }
--- src/api/database/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'connection.php';
-
-try {
-    $db = get_connection();
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
