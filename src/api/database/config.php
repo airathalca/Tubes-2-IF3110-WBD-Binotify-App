8f93cb0eb36adc6ed881039399049ecc4499464b
rayhankinan <rayhankinan@gmail.com>
2022-10-21 18:08:24 +0700

    feat: add docker compose

--- src/api/database/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$host = $_ENV['DB_HOST'];
-$dbname = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
---
+++ src/database/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$host = $_ENV['MYSQL_HOST'];
+$dbname = $_ENV['MYSQL_NAME'];
+$user = $_ENV['MYSQL_USER'];
+$password = $_ENV['MYSQL_PASSWORD'];
