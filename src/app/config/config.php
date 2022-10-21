4dc70918692d7166967074d2a87e64856f4a254f
rayhankinan <rayhankinan@gmail.com>
2022-10-21 21:45:55 +0700

    fix: fix database connection

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -4,3 +4,4 @@ define('HOST', $_ENV['MYSQL_HOST']);
 define('DBNAME', $_ENV['MYSQL_DATABASE']);
 define('USER', $_ENV['MYSQL_USER']);
 define('PASSWORD', $_ENV['MYSQL_PASSWORD']);
+define('PORT', $_ENV['MYSQL_PORT']);
--- src/app/core/database.php
+++ src/app/core/database.php
@@ -1,22 +1,27 @@
 <?php
-class Database {
+
+require_once 'tables.php';
+
+class Database
+{
     private $host = HOST;
     private $db_name = DBNAME;
     private $user = USER;
     private $password = PASSWORD;
+    private $port = PORT;
     private $db_connection;
 
     public function __construct()
     {
         global $song_table, $album_table, $user_table;
-        $dsn = "mysql:host=$this->host;dbname=$this->db_name;charset=utf8";
+        $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->db_name";
         try {
             $this->db_connection = new PDO($dsn, $this->user, $this->password);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             die($e->getMessage());
         }
         $this->db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         $this->db_connection->exec($user_table);
         $this->db_connection->exec($album_table);
         $this->db_connection->exec($song_table);
