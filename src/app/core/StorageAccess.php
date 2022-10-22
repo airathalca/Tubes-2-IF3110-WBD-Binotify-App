f76c272012ca2f5de45610ee476a7994c97b393d
rayhankinan <rayhankinan@gmail.com>
2022-10-22 23:11:10 +0700

    feat: add storage access library

--- src/app/core/StorageAccess.php
+++ src/app/core/StorageAccess.php
@@ -2,4 +2,40 @@
 
 class StorageAccess
 {
+    private $storageDir;
+
+    public const IMAGE_PATH = 'image';
+    public const SONG_PATH = 'song';
+
+    public function __construct($foldername)
+    {
+        $this->storageDir = __DIR__ . '/../../storage/' . $foldername . '/';
+    }
+
+    public function createFile($data)
+    {
+        $valid = false;
+        $filename = '';
+
+        while (!$valid) {
+            $filename = uniqid("", true);
+
+            if (!file_exists($this->storageDir . $filename)) {
+                $valid = true;
+            }
+        }
+
+        file_put_contents($this->storageDir . $filename, $data);
+        return $filename;
+    }
+
+    public function deleteFile($filename)
+    {
+        if (file_exists($$this->storageDir . $filename)) {
+            unlink($this->storageDir . $filename);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -2,4 +2,8 @@
 
 require_once __DIR__ . '/../app/init.php';
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 $app = new App();
