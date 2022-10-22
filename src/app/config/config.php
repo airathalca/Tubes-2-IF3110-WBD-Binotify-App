d65086e91c9ec3053a82cb9ba3cb7767ccf23e50
rayhankinan <rayhankinan@gmail.com>
2022-10-23 05:11:50 +0700

    feat: add icon to view

--- src/app/config/config.php
+++ src/app/config/config.php
@@ -1,14 +1,22 @@
 <?php
 
+// url
+define('BASE_URL', 'http://localhost/public');
+
+// database
 define('HOST', $_ENV['MYSQL_HOST']);
 define('DBNAME', $_ENV['MYSQL_DATABASE']);
 define('USER', $_ENV['MYSQL_USER']);
 define('PASSWORD', $_ENV['MYSQL_PASSWORD']);
 define('PORT', $_ENV['MYSQL_PORT']);
+
+// file
 define('MAX_SIZE', 10 * 1024 * 1024);
 define('ALLOWED_FILES', [
     'image/jpeg' => '.jpeg',
     'image/png' => '.png',
     'audio/mpeg' => '.mp3'
 ]);
+
+// bcrypt
 define('BCRYPT_COST', 10);
--- src/app/controllers/user.php
+++ src/app/controllers/user.php
@@ -4,6 +4,22 @@ class User extends Controller implements DefaultMethodInterface
 {
     public function index()
     {
-        echo 'Halo saya Kinan.';
+        $this->view('user/index');
+    }
+
+    public function login()
+    {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    break;
+                case 'POST':
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
     }
 }
