812019be10ec6123e80a43a64df21334a93140b0
rayhankinan <rayhankinan@gmail.com>
2022-10-23 07:12:19 +0700

    fix: logo

--- src/app/components/user/LoginPage.php
+++ src/app/components/user/LoginPage.php
@@ -4,7 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="<?= BASE_URL ?>/images/icon/favicon.ico">
+    <link rel="apple-touce-icon" sizes="180x180" href="<?= BASE_URL ?>/images/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/images/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>/images/icon/favicon-16x16.png">
+    <link rel="manifest" href="<?= BASE_URL ?>/images/icon/site.webmanifest">
     <link rel="stylesheet" type="text/css" href="<?= BASE_URL ?>/styles/login.css">
     <script type="text/javascript" src="<?= BASE_URL ?>/javascript/login.js" defer></script>
     <title> Login </title>
