22e2e2035274c9e671b6850ad4bbf122b1255ce6
rayhankinan <rayhankinan@gmail.com>
2022-10-24 03:48:06 +0700

    fix: minor session

--- src/public/index.php
+++ src/public/index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../app/init.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
-    $current_time = time();
 
+    $current_time = time();
     $_SESSION['created_at'] = $current_time;
     $_SESSION['updated_at'] = $current_time;
 }
@@ -13,7 +13,8 @@ if (session_status() === PHP_SESSION_NONE) {
 if (session_status() === PHP_SESSION_ACTIVE) {
     $current_time = time();
 
-    if ($current_time - $_SESSION['created_at'] > SESSION_REGENERATION_TIME) {
+    if ($current_time - $_SESSION['updated_at'] > SESSION_REGENERATION_TIME) {
+        // Prevent Session Fixation Attacks
         session_regenerate_id(true);
         $_SESSION['updated_at'] = $current_time;
     }
