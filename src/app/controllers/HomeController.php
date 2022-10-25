35014d881d1cc69946102ddc11b609e15f937151
airathalca <13520101@std.stei.itb.ac.id>
2022-10-25 11:37:50 +0700

    add method search

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -27,4 +27,39 @@ class HomeController extends Controller implements ControllerInterface
             http_response_code($e->getCode());
         }
     }
+    public function search() {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $q = '';
+                    $sort = 'judul';
+                    $filter = 'all';
+
+                    if (isset($_GET['q'])) {
+                        $q = $_GET['q'];
+                    }
+                    if (isset($_GET['filter'])) {
+                        $filter = $_GET['filter'];
+                    }
+                    if (isset($_GET['sort'])) {
+                        $page = $_GET[''];
+                    }
+                    $songModel = $this->model('SongModel');
+                    $songArr = $songModel->getByQuery($q, $sort, $filter);
+                    if (isset($_SESSION['user_id'])) {
+                        $userModel = $this->model('UserModel');
+                        $user = $userModel->getUserFromID($_SESSION['user_id']);
+                        $searchView = $this->view('home', 'SearchView', ['username' => $user->username, 'song_arr' => $songArr]);
+                    } else {
+                        $searchView = $this->view('home', 'SearchView', ['username' => null, 'song_arr' => $songArr]);
+                    }
+                    $searchView->render();
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
 }
