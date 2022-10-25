abdcc9a2be1ff4a502b6c741938c284c385f8266
airathalca <13520101@std.stei.itb.ac.id>
2022-10-25 18:31:23 +0700

    move function search from home to song controller

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -27,42 +27,5 @@ class HomeController extends Controller implements ControllerInterface
             http_response_code($e->getCode());
         }
     }
-    public function search() {
-        try {
-            switch ($_SERVER['REQUEST_METHOD']) {
-                case 'GET':
-                    $q = '';
-                    $sort = 'judul';
-                    $filter = 'all';
-
-                    if (isset($_GET['q'])) {
-                        $q = $_GET['q'];
-                    }
-                    if (isset($_GET['filter'])) {
-                        $filter = $_GET['filter'];
-                    }
-                    if (isset($_GET['sort'])) {
-                        $page = $_GET['sort'];
-                    }
-                    $songModel = $this->model('SongModel');
-                    $genreArr = $songModel->getGenre();
-                    $songArr = $songModel->getByQuery($q, $sort, $filter);
-                    if (isset($_SESSION['user_id'])) {
-                        $userModel = $this->model('UserModel');
-                        $user = $userModel->getUserFromID($_SESSION['user_id']);
-                        $searchView = $this->view('home', 'SearchView', ['username' => $user->username, 'is_admin' => $user->is_admin, 
-                        'genre_arr' => $genreArr, 'song_arr' => $songArr]);
-                    } else {
-                        $searchView = $this->view('home', 'SearchView', ['username' => null, 'is_admin' => false, 
-                        'genre_arr' => $genreArr, 'song_arr' => $songArr]);
-                    }
-                    $searchView->render();
-                    break;
-                default:
-                    throw new LoggedException('Method Not Allowed', 405);
-            }
-        } catch (Exception $e) {
-            http_response_code($e->getCode());
-        }
-    }
+    
 }
--- src/app/controllers/SongController.php
+++ src/app/controllers/SongController.php
@@ -6,4 +6,42 @@ class SongController extends Controller implements ControllerInterface
     {
         echo 'La la la';
     }
+    public function search() {
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    $q = '';
+                    $sort = 'judul';
+                    $filter = 'all';
+
+                    if (isset($_GET['q'])) {
+                        $q = $_GET['q'];
+                    }
+                    if (isset($_GET['filter'])) {
+                        $filter = $_GET['filter'];
+                    }
+                    if (isset($_GET['sort'])) {
+                        $sort = $_GET['sort'];
+                    }
+                    $songModel = $this->model('SongModel');
+                    $genreArr = $songModel->getGenre();
+                    $songArr = $songModel->getByQuery($q, $sort, $filter);
+                    if (isset($_SESSION['user_id'])) {
+                        $userModel = $this->model('UserModel');
+                        $user = $userModel->getUserFromID($_SESSION['user_id']);
+                        $searchView = $this->view('song', 'SearchView', ['username' => $user->username, 'is_admin' => $user->is_admin, 
+                        'genre_arr' => $genreArr, 'song_arr' => $songArr]);
+                    } else {
+                        $searchView = $this->view('song', 'SearchView', ['username' => null, 'is_admin' => false, 
+                        'genre_arr' => $genreArr, 'song_arr' => $songArr]);
+                    }
+                    $searchView->render();
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+        }
+    }
 }
