0d937d7ee8c590c4984058e8c535634e20f239c9
rayhankinan <rayhankinan@gmail.com>
2022-10-28 14:50:15 +0700

    fix: delete crsf_token in song limit

--- src/app/middlewares/SongLimitMiddleware.php
+++ src/app/middlewares/SongLimitMiddleware.php
@@ -2,20 +2,18 @@
 
 class SongLimitMiddleware
 {
-    public function checkSong($csrf_token)
+    public function checkSong()
     {
         if ($_SESSION['song_count'] >= MAX_SONG_COUNT) {
             return false;
         }
-        $_SESSION['token_limit'] = $csrf_token;
         $_SESSION['song_count'] += 1;
         return true;
     }
 
-    public function makeNewSession($csrf_token)
+    public function makeNewSession()
     {
         $_SESSION['song_count'] = 0;
-        $_SESSION['token_limit'] = $csrf_token;
         $_SESSION['date'] = date('Y-m-d');
     }
 }
