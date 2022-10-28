3e9ffca2e7f3ffb81ac8875bae3d9a70801d25d0
acomarcho <acoxstpd@gmail.com>
2022-10-28 07:50:57 +0700

    Make custom audio player for user song detail page

--- src/app/components/song/UserSongDetailPage.php
+++ src/app/components/song/UserSongDetailPage.php
@@ -59,9 +59,24 @@
                     <?php } ?>
                     <div class="audio-player-container">
                         <p class="audio-info"><?= $this->data['judul'] ?> by <?= $this->data['penyanyi'] ?></p>
-                        <audio controls class="audio-player">
-                            <source src="<?= STORAGE_URL?>/songs/<?=$this->data['audio_path']?>">
-                        </audio>
+                        <div class="player-button">
+                            <div class="button-play">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="#000000" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+                            </div>
+                            <div class="button-pause">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="#000000" d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+                            </div>
+                        </div>
+                        <div class="progress-bar-container">
+                            <p id="curr-duration">0:00</p>
+                            <input type="range" name="progress-bar" id="progress-bar" step="0.01" value="0">
+                            <p id="final-duration"><?= floor($this->data['duration']/60) ?>:<?= str_pad($this->data['duration'] % 60, 2, "0") ?></p>
+                        </div>
+                        <div class="hide-player">
+                            <audio controls class="audio-player">
+                                <source src="<?= STORAGE_URL?>/songs/<?=$this->data['audio_path']?>">
+                            </audio>
+                        </div>
                     </div>
                 <?php } else { ?>
                     <p class="info">Cannot find the song you're looking for!</p>
--- src/public/javascript/song/play-song.js
+++ src/public/javascript/song/play-song.js
@@ -1,6 +1,41 @@
 const audioPlayer = document.querySelector('.audio-player');
 
+const playButton = document.querySelector('.button-play');
+const pauseButton = document.querySelector('.button-pause');
+const progressBar = document.querySelector('#progress-bar');
+const currDuration = document.querySelector("#curr-duration");
+const finalDuration = document.querySelector("#curr-duration");
+
+function padDigits(number, digits) {
+    return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
+}
+
 if (audioPlayer) {
+    playButton.addEventListener('click', () => {
+        audioPlayer.play();
+    })
+
+    pauseButton.addEventListener('click', () => {
+        audioPlayer.pause();
+    })
+
+    audioPlayer.addEventListener('timeupdate', () => {
+        const percent = audioPlayer.currentTime / audioPlayer.duration * 100;
+        
+        progressBar.value = percent;
+
+        const seconds = Math.floor(audioPlayer.currentTime);
+
+        currDuration.textContent = `${Math.floor(seconds / 60)}:${padDigits(seconds % 60, 2)}`;
+    })
+
+    progressBar.addEventListener('input', (e) => {
+        const seconds = e.target.value / 100 * audioPlayer.duration;
+        currDuration.textContent = `${Math.floor(seconds / 60)}:${padDigits(Math.floor(seconds) % 60, 2)}`;
+
+        audioPlayer.currentTime = seconds;
+    });
+
     audioPlayer.addEventListener('play', () => {
         if (username === '') {
             if (SONG_COUNT >= MAX_SONG_COUNT) {
