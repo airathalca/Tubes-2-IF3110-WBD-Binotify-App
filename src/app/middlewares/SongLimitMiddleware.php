afadb229da293995a04d53256f2a0d653888f81f
rayhankinan <rayhankinan@gmail.com>
2022-10-28 13:45:11 +0700

    refactor: screenshot and readme

--- src/app/middlewares/SongLimitMiddleware.php
+++ src/app/middlewares/SongLimitMiddleware.php
@@ -4,9 +4,8 @@ class SongLimitMiddleware
 {
     public function checkSong($csrf_token)
     {
-        if ($csrf_token !== $_SESSION['token_limit'])
-        {
-            if($_SESSION['song_count'] >= MAX_SONG_COUNT) {
+        if ($csrf_token !== $_SESSION['token_limit']) {
+            if ($_SESSION['song_count'] >= MAX_SONG_COUNT) {
                 return false;
             }
             $_SESSION['token_limit'] = $csrf_token;
