a80170b9e2354016a52fd61941c6f911da62a1b1
acomarcho <acoxstpd@gmail.com>
2022-10-28 08:05:55 +0700

    Remove song title from audio player to save some vertical space

--- src/app/components/song/UserSongDetailPage.php
+++ src/app/components/song/UserSongDetailPage.php
@@ -58,7 +58,7 @@
                         <a href="<?= BASE_URL?>/album/detail/<?= $this->data['album']?>" class="button button-album">See album!</a>
                     <?php } ?>
                     <div class="audio-player-container">
-                        <p class="audio-info"><?= $this->data['judul'] ?> by <?= $this->data['penyanyi'] ?></p>
+                        <!-- <p class="audio-info"><?= $this->data['judul'] ?> by <?= $this->data['penyanyi'] ?></p> -->
                         <div class="player-button">
                             <div class="button-play">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path fill="#000000" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
