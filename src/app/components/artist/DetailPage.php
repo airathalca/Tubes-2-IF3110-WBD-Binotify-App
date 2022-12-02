928b3a01f0986ea5ddf723154864562531e8b606
acomarcho <acoxstpd@gmail.com>
2022-12-02 12:54:44 +0700

    Fix: USER_ID at DetailPage.php

--- src/app/components/artist/DetailPage.php
+++ src/app/components/artist/DetailPage.php
@@ -19,7 +19,7 @@
     <script type="text/javascript" defer>
         const CSRF_TOKEN = "<?= $_SESSION['csrf_token'] ?? '' ?>";
         const USERNAME = "<?= $this->data['username'] ?? '' ?>";
-        const USER_ID = "<?= $this->data['user_id'] ?? '' ?>";
+        const USER_ID = "<?= $_SESSION['user_id'] ?? '' ?>";
         const ARTIST_ID = <?= $this->data['artist_ID'] ?>;
         const REST_URL = "<?= REST_URL ?>";
     </script>
